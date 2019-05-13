7b27c0f86d5268739436be4017595a97e4a62a50
mpowloka <michalpowloka@gmail.com>
2019-05-13 08:47:43 +0200

    Test of const classes and variables (const is not actually optional?)

--- lib/const_test.dart
+++ lib/const_test.dart
@@ -1,25 +1,61 @@
 import 'package:flutter/material.dart';
 
-class ConstTest extends StatelessWidget {
+class ConstTest extends StatefulWidget {
+
+  @override
+  _ConstTestState createState() => _ConstTestState();
+
+}
+
+class _ConstTestState extends State<ConstTest> {
+
+
+  final array1 = const [1, 2, 3];
+
+  var array2 = const [1, 2, 3];
+
+  static const array3 = [1, 2, 3];
+
+  final array4 = [1, 2, 3];
+
+  var array5 = [1, 2, 3];
+
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
       child: Text("Const test"),
       onPressed: () {
-        var a = const _BigConstClass();
-        var b = const _BigConstClass();
-        var c = _BigConstClass();
+        testConstClass();
 
-        print("a == b = ${a == b}"); //true
-
-        print("a == c = ${a == c}"); //false
+        testConstFields();
       },
     );
+  }
+
+  void testConstClass() {
+    var a = const _BigConstClass();
+    var b = const _BigConstClass();
+    var c = _BigConstClass();
+
+    print("a == b = ${a == b}"); //true
+
+    print("a == c = ${a == c}"); //false
+  }
+
+  void testConstFields() {
+
+    print("array1 == array2 = ${array1 == array2}"); //true
+    print("array1 == array3 = ${array1 == array3}"); //true
+    print("array1 == array4 = ${array1 == array4}"); //false
+    print("array1 == array5 = ${array1 == array5}"); //false
+
+    print("array4 == array5 = ${array4 == array5}"); //false
+
+
   }
 }
 
 class _BigConstClass {
-
   final int _finalValue = 42;
 
   final _finalString = "Const string";
@@ -34,17 +70,13 @@ class _BigConstClass {
 }
 
 class _SmallConstClass {
-
   final int _value;
 
   const _SmallConstClass(this._value);
-
 }
 
 class _NonConstClass {
-
   int _value;
 
   _NonConstClass(this._value);
-
 }
